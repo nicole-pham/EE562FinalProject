124351103ac39314943f625a77306e0d2c281761
Nicole Pham <nipha@uw.edu>
2023-12-03 10:56:48 -0800

    Add model loading file
    TODO: Fix Efficient Transformer. Refactor predict file.

--- code/main.py
+++ code/main.py
@@ -47,6 +47,9 @@ def train(dataloader, model, loss_fn, optimizer, success_metric):
             loss, current = loss.item(), (batch + 1) * len(X)
             score = success_metric(pred, y)
             print(f"loss: {loss:>7f}  score: {score:>7f}  [{current:>5d}/{size:>5d}]")
+            
+    # from https://github.com/Eladamar/fast_scnn/blob/master/main.py
+    torch.save(model.state_dict(), 'checkpoints/' + model.__class__.__name__ + datetime.datetime.today().strftime("%m_%d"))
 
 def test(dataloader, model, loss_fn, success_metric):
     size = len(dataloader.dataset)
@@ -60,14 +63,13 @@ def test(dataloader, model, loss_fn, success_metric):
             if isinstance(pred, (list, tuple)):
                 for i in range(len(pred)):
                     test_loss += loss_fn(pred[i], y).item()
-            break
     test_loss /= num_batches
     score = success_metric(pred, y)
     print(f"Test Error: \n Accuracy: {(100*correct):>0.1f}%, Avg loss: {test_loss:>8f} \n score: {score:>7f} \n")
 
 # code heavily borrowed from https://github.com/Eladamar/fast_scnn/blob/master/main.py
-epochs = 100 # changed epochs from 100 to 10 for time
-batch_size = 2 # increased batch size (images are 256x256 instead of 6000x6000)
+epochs = 1 # changed epochs from 100 to 10 for time
+batch_size = 8 # increased batch size (images are 256x256 instead of 6000x6000)
 learning_rate = 1e-3
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 torch.cuda.empty_cache()
@@ -86,7 +88,6 @@ dl_test = DataLoader(ds_test, batch_size, shuffle=True)
 # Code reference: https://pytorch.org/tutorials/beginner/basics/quickstart_tutorial.html
 #model = FastSCNN(num_classes=6)
 
-
 model = swinT(nclass=6, pretrained=True, aux=True, head="mlphead", edge_aux=False)
 
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
@@ -98,6 +99,6 @@ model.to(device)
 for epoch in range(epochs):
     if epoch % 10 == 0:
         print('epoch', epoch+1, 'of', epochs)
-        #train(dl_train, model, loss_fn, optimizer, success_metric)
+        train(dl_train, model, loss_fn, optimizer, success_metric)
         test(dl_test, model, loss_fn, success_metric)
 
---
+++ code/predict.py
@@ -0,0 +1,119 @@
+import os
+import argparse
+import torch
+
+from FastSCNN import FastSCNN
+from models.swinT import swin_tiny as swinT
+
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.colors import ListedColormap
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+
+from dataset import RandomHorizontalFlip
+
+from torchvision import transforms
+
+# Taken from https://github.com/Eladamar/fast_scnn/blob/master/metrics.py
+def pixel_accuracy(preds, mask):
+    preds = preds[0]
+    pred_argmax = torch.argmax(preds, axis=1)
+    mask_argmax = torch.argmax(mask, axis=1)
+    acc_sum = torch.sum(pred_argmax == mask_argmax).item()
+    # divide by batch size
+    acc_mean = acc_sum / preds.shape[0]
+    image_size = preds.shape[2] * preds.shape[3]
+    acc = float(acc_mean) / (image_size + 1e-10)
+    return acc
+
+parser = argparse.ArgumentParser(
+    description='Predict segmentation result from a given image')
+parser.add_argument('--checkpoint', type=str, default='checkpoints/SwinT12_03',
+                    help='which checkpoint to use')
+parser.add_argument('--input_image', type=str,
+                    help='path to the input picture')
+parser.add_argument('--outdir', default='data/test_result', type=str,
+                    help='path to save the predict result')
+parser.add_argument('--num_classes', type=int, default=6,
+                    help='num of classes in model')
+parser.add_argument('--eval', default=None, type=str,
+                    help='image label for evaluation score')
+
+
+args = parser.parse_args()
+
+def predict():
+    #device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    device = 'cpu'
+    # output folder
+    if not os.path.exists(args.outdir):
+        os.makedirs(args.outdir)
+
+    #img = np.load(args.input_image)
+    img = np.load('img-0a0d7c9d-1158-4d74-8c6b-6f536ebca85f.npy')
+    mask = np.load('img-0a0d7c9d-1158-4d74-8c6b-6f536ebca85f-mask.npy')
+    
+    affine_transform = transforms.Compose([
+        RandomHorizontalFlip(1)
+    ])
+    
+    normal_transform = transforms.Compose([
+        transforms.Normalize((0.5,0.5,0.5),(0.5,0.5,0.5))
+    ])
+        
+        
+    transformed_img, transformed_mask = affine_transform((img, mask))
+    transformed_img = torch.tensor(transformed_img.copy() / 255.0)
+    transformed_img = normal_transform(transformed_img)
+    transformed_mask = torch.tensor(transformed_mask.copy(), dtype=torch.float64)
+    
+    fig = plt.figure(1)
+    canvas = FigureCanvas(fig)
+    plt.imshow(torch.transpose(torch.transpose(transformed_img, 0, 2), 0, 1))
+    canvas.print_figure('test-pic.png')
+    
+    #model = FastSCNN(args.num_classes).to(device)
+    model = swinT(nclass=args.num_classes, pretrained=True, aux=True, head="mlphead", edge_aux=False)
+
+    model.load_state_dict(torch.load(args.checkpoint, map_location=device))
+    # model = torch.load(args.checkpoint, map_location=device)
+    print('Finished loading model!')
+    model.eval()
+    model.double()
+    with torch.no_grad():
+        outputs = model(transformed_img.unsqueeze(0).to(device))
+    pred = torch.argmax(outputs[0], 1).squeeze(0).to(device)
+    
+    transformed_mask = np.argmax(transformed_mask, 0)
+    
+    ClassesColors = {
+        (255,0,0):0,
+        (255,255,255):1,
+        (255,255,0):2,
+        (0,0,255):3,
+        (0,255,255):4,
+        (0,255,0):5
+    }
+    
+    Class1H2RGB =  dict([[str(val),key] for key,val in ClassesColors.items()])
+    cmap = ListedColormap(np.array([Class1H2RGB[k] for k in sorted(Class1H2RGB.keys())])/255.0)
+    fig = plt.figure(2)
+    canvas = FigureCanvas(fig)
+    plt.imshow(transformed_mask,cmap=cmap,rasterized=True)
+    canvas.print_figure('test-mask.png')
+    fig = plt.figure(3)
+    canvas = FigureCanvas(fig)
+    plt.imshow(pred, cmap=cmap, rasterized=True)
+    canvas.print_figure('test-pred.png')
+    plt.show(block=True)
+
+    if not args.eval is None:
+        # only one label for now, so add batch=1 dim
+        label = torch.load(args.eval).unsqueeze(0)
+        output = outputs[0]
+        print("image score: ", pixel_accuracy(output, label))
+
+
+if __name__ == '__main__':
+    predict()
+
